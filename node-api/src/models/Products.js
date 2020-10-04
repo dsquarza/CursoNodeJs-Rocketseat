0a422ec8a5c5efb39b0118877eab9a5386b64491
André Squarça <66263898+dsquarza@users.noreply.github.com>
2020-10-04 18:39:34 -0300

    Add files via upload

---
+++ node-api/src/models/Products.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const ProductScheema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    cretedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+ProductScheema.plugin(mongoosePaginate);
+mongoose.model('Product', ProductScheema);
